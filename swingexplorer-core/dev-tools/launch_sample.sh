a50ad283cbca5887c42020cb8b69bce84854a55a
Andrew Janke <andrew@apjanke.net>
2018-06-28 23:40:36 -0400

    launch_sample.sh: fix paths

--- swingexplorer-core/dev-tools/launch_sample.sh
+++ swingexplorer-core/dev-tools/launch_sample.sh
@@ -8,7 +8,11 @@
 # in your local Maven repo, and swingexplorer-core built with `mvn package`. 
 
 M2_REPO=$HOME/.m2/repository
-VERSION=1.7.0
+VERSION=1.7.0-SNAPSHOT
+
+this_dir=$(dirname $0)
+this_dir=$(realpath $this_dir)
+core_dir=$(dirname $this_dir)
 
 JAVA_CLASSPATH=$M2_REPO/org/swingexplorer/swingexplorer-agent/${VERSION}/swingexplorer-agent-${VERSION}.jar:$M2_REPO/org/jdesktop/swing-layout/1.0.3/swing-layout-1.0.3.jar:$M2_REPO/javassist/javassist/3.12.1.GA/javassist-3.12.1.GA.jar
-java -cp $JAVA_CLASSPATH:../target/swingexplorer-core-${VERSION}.jar org.swingexplorer.Launcher sample.FRMPerson
+java -cp $JAVA_CLASSPATH:${core_dir}/target/swingexplorer-core-${VERSION}.jar org.swingexplorer.Launcher sample.FRMPerson
