80566961d43e826f5bd878bcf476200deb91160d
touchmii <ipeooc@gmail.com>
2021-07-28 17:14:04 +0800

    init commit

--- swingexplorer-core/src/main/java/com/example/PersonEditorWithSwingExplorer.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class PersonEditorWithSwingExplorer {
-    public static void main(String args[]) {
-        org.swingexplorer.Launcher.main(new String[] {"com.example.PersonEditor"});
-    }
-}
